92bb3f47add0a43c15e0f8211cc5db8ff548f015
unknown <marawan1132003@gmail.com>
2023-05-14 23:01:40 +0300

    Added

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/system'
+
+import Exercises from '../components/Exercises'
+import SearchExercises from '../components/SearchExercises'
+import HeroBanner from '../components/HeroBanner'
+
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
